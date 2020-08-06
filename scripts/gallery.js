3e0663efa39360b8b552cf33a81b0e506ec28b6f
n8body <1971n8b8dy@users.noreply.github.com>
2020-08-06 22:56:28 +0300

    0.1.2
    Some new pages
    Lighter images

---
+++ scripts/gallery.js
@@ -0,0 +1,42 @@
+// SLider
+let mySwiper = new Swiper('.swiper-container', {
+  direction: 'horizontal',
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+  },
+  effect: 'slide',
+  centeredSlides: true,
+  spaceBetween: 10,
+  autoplay: {
+    delay: 5000,
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+  preloadImages: false,
+  lazy: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    960: {
+      slidesPerView: 2,
+    },
+    1280: {
+      slidesPerView: 3,
+    }
+  }
+});
+document.getElementsByClassName('swiper-container')[0].addEventListener('mouseenter', function() {
+  mySwiper.autoplay.stop();
+});
+
+document.getElementsByClassName('swiper-container')[0].addEventListener('mouseleave',function() {
+  mySwiper.autoplay.start();
+});
--- scripts/script.js
+++ scripts/script.js
@@ -3,11 +3,44 @@ AOS.init({
   once: true
 });
 
-// Menu
-window.addEventListener('scroll', function() {
-  showBackToTopButton();
+// Check for touchscreen
+window.addEventListener('load', function() {
+  function is_touch_device() {
+    let prefixes = ' -webkit- -moz- -o- -ms- '.split(' ');
+    let mq = function (query) {
+        return window.matchMedia(query).matches;
+    }
+    if (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
+        return true;
+    }
+    let query = ['(', prefixes.join('touch-enabled),('), 'heartz', ')'].join('');
+    return mq(query);
+  }
+
+  if (document.documentElement.clientWidth > 960 || !is_touch_device()) {return}
+    let drops = document.getElementsByClassName('dropdown'),
+      dropsbtns = document.getElementsByClassName('dropbtn');
+    for (let i = 0; i < drops.length; i++) {
+      drops[i].classList.remove('not-mobile');
+
+      dropsbtns[i].addEventListener('click', function() {
+        if (drops[i].dataset.isopen == 'false') {
+
+          this.nextElementSibling.style.display = 'block';
+
+          drops[i].dataset.isopen = 'true';
+
+        } else {
+
+          this.nextElementSibling.style.display = 'none';
+          
+          drops[i].dataset.isopen = 'false';
+        }
+      });
+    }
 });
 
+// Menu
 let menuIcon = document.getElementById('menu-icon'),
     navbar = document.getElementById('navigation');
 
@@ -18,6 +51,10 @@ menuIcon.addEventListener('click', function() {
 
 // Back to top
 let backBtn = document.getElementById('back-to-top');
+
+window.addEventListener('scroll', function() {
+  showBackToTopButton();
+});
 
 function showBackToTopButton() {
   if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
@@ -30,5 +67,5 @@ function showBackToTopButton() {
 backBtn.addEventListener('click', function() {
   $('html, body').animate({
     scrollTop: 0
-  }, 500);
+  }, 400);
 });
