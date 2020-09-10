5fe06d9942e2ded2e4f3150d049797b9f8419116
n8body <1971n8b8dy@users.noreply.github.com>
2020-09-10 08:18:15 +0300

    Update loading.js

--- scripts/loading.js
+++ scripts/loading.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', function() {
+window.addEventListener('DOMContentLoaded', function() {
   let loaderBlock = document.getElementById('loader-block');
   setTimeout(() => {loaderBlock.style = 'opacity: 0; z-index: -1;'; document.body.style.overflow = 'visible'}, 300);
 	setTimeout(() => loaderBlock.remove(), 500)
